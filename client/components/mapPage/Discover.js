5e70495a917957b3dba783cd7e30f4dc04ae8827
szymonzawrotny <szymonzawrotny@gmail.com>
2024-10-22 19:54:02 +0200

    post template ready

--- client/components/mapPage/Discover.js
+++ client/components/mapPage/Discover.js
@@ -1,4 +1,6 @@
 import "@/styles/mapPage/interface/panels/discover.scss"
+import { SlOptions } from "react-icons/sl";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Discover = ()=>{
     return(
@@ -7,7 +9,18 @@ const Discover = ()=>{
                 search
             </div>
             <div className="eventList">
-                event list
+                <div className="event">
+                    <div className="options"><SlOptions size={28}/></div>
+                    <div className="userData">
+                        <div className="icon"><FaRegUserCircle/></div>
+                        <div className="name">szymonzawrotny@gmail.com</div>
+                    </div>
+                    <div className="photos">2</div>
+                    <div className="title">3</div>
+                </div>
+                <div className="event">dwa</div>
+                <div className="event">trzy</div>
+                <div className="event">cztery</div>
             </div>
         </div>
     )
